a426598b5f2763cd4fd093b4f2e72dd57e169c33
KUMAR NISHANT <161965319+K-Nishant-18@users.noreply.github.com>
2025-01-03 16:59:59 +0530

    Delete 2264-minimum-sum-of-four-digit-number-after-splitting-digits directory

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int minimumSum(int num) {
-        char[] ch=(num+"").toCharArray();
-        Arrays.sort(ch);
-        int n=Integer.parseInt(""+ch[0]+ch[2]);
-        int m=Integer.parseInt(""+ch[1]+ch[3]);
-        return n+m;
-    }        
-}
