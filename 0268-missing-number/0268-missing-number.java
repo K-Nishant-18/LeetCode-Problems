eb02ea1ad06639c6df9e1147ba125c0945e0ddec
KUMAR NISHANT <161965319+K-Nishant-18@users.noreply.github.com>
2025-01-26 23:17:31 +0530

    Update 0268-missing-number.java

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -9,3 +9,23 @@ class Solution {
         return (rangeSum-actualSum);
     }
 }
+
+
+/* 
+
+====== Alternate Solution using XOR ======
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int xor=0;
+        for(int i=0; i<=n; i++){
+            xor=xor^i;
+        }
+        for(int x:nums){
+            xor=xor^x;
+        }
+        return xor;
+    }
+}
+*/
