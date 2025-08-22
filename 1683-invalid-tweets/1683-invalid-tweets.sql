447c0b2c0a5b1e9a64b1cfbf05d9c03edac478e8
KUMAR NISHANT <me.knishant@gmail.com>
2025-08-22 22:14:39 +0530

    Time: 566 ms (73.27%), Space: 0B (100%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,14 +1,4 @@
-//My solution
-SELECT tweet_id 
-FROM Tweets
-WHERE CHAR_LENGTH(content)>15;
-
-// Other solution
+# Write your MySQL query statement below
 SELECT tweet_id 
 FROM Tweets
 WHERE LENGTH(content)>15;
-
-/*
-  If your tweets are ASCII-only (English letters, numbers, symbols), then LENGTH() and CHAR_LENGTH() give the same result — but LENGTH() is a little faster.
-  If your tweets may contain Unicode characters, then LENGTH() may give the wrong result (too big).
-/*
