c875ede1a68a4a70b9006321e433367515c9cf0f
KUMAR NISHANT <me.knishant@gmail.com>
2026-07-16 10:55:24 +0530

    Time: 1 ms (41.09%), Space: 63.5 MB (8.04%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,47 +1,14 @@
-/*
-MY PREVIOUS SOLUTION
-
 class Solution {
-    public void rotate(int[] nums, int k) {
-    int n=nums.length;
-    k=k%n;
-    int[] rotated=new int[n];
-    for (int i=0; i<n; i++){
-        int key=(i+k)%n;
-        rotated[key]=nums[i];
-    } 
-    for(int i=0; i<n; i++){
-        nums[i]=rotated[i];
-    } 
-    }
-}
-
-*/
-
-
-
-// UPDATED SOLUTION WITH BETTER APPROACH
-
-class Solution {
-    public void rotate(int[] nums, int k) {
-        int n=nums.length;
-        k=k%n;
-        
-        reverse(nums, 0, n-k-1);
-        reverse(nums, n-k, n-1);
-        reverse(nums, 0, n-1);
-
-    }
-
-    void reverse(int[] nums, int i, int j){
-            int temp;
-            for(int k=i; k<j; k++){
-                temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                i++;
-                j--;
-            }
+       public void rotate(int[] nums, int k) {
+        int l=nums.length;
+        k=k%l;
+        int[] temp=new int[l];
+        for(int i=0; i<l; i++){
+            int m= (i+k)%l;
+            temp[m]=nums[i];
         }
-
+        for(int j=0; j<l; j++){
+            nums[j]=temp[j];
+        }
+    }
 }
