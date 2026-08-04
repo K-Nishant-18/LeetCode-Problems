6d84e5aa0a2db568eae98a959e43340834b95ef4
KUMAR NISHANT <me.knishant@gmail.com>
2026-08-05 02:02:58 +0530

    Time: 4 ms (36.84%), Space: 43.4 MB (41.57%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,16 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
-        // mapping: closing bracket -> required opening bracket
-        Map<Character, Character> pairs = new HashMap<>();
-        pairs.put(')', '(');
-        pairs.put(']', '[');
-        pairs.put('}', '{');
-
-        Stack<Character> stack=new Stack<>();
-        int length=s.length();
-        for(int i=0; i<=length-1; i++){
-            if(pairs.containsKey(s.charAt(i))){
-                if(stack.isEmpty() || stack.peek() != pairs.get(s.charAt(i))){
-                    return false;
-                }else{
-                    stack.pop();
-                }
-            }else{
-                stack.push(s.charAt(i));
-            }
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
         }
+        
         return stack.isEmpty();
     }
 }
