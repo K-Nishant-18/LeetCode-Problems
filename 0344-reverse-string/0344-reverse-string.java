fdd1c664c29770297b1e41a8c0bc01ec75052065
KUMAR NISHANT <me.knishant@gmail.com>
2026-08-04 08:51:03 +0530

    Time: 0 ms (100%), Space: 48.1 MB (75%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n=s.length;
+        char temp;
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
